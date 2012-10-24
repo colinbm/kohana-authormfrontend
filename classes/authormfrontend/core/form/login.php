cff5d26711e9a6ad6aa7663bf26cc82fa6e06d95
Colin Burn-Murdoch <colin@burn-murdoch.com>
2012-10-24 15:27:40 +0100

    Add remember field and pass that to $auth->login

--- classes/authormfrontend/core/form/login.php
+++ classes/authormfrontend/core/form/login.php
@@ -5,6 +5,7 @@ class AuthOrmFrontend_Core_Form_Login extends FormManager {
 	protected function setup() {
 		$this->add_field('username');
 		$this->add_field('password', array('display_as' => 'password'));
+		$this->add_field('remember', array('display_as' => 'bool', 'label' => 'Remember me?'));
 		$this->rule('username', 'not_empty');
 		$this->rule('password', 'not_empty');
 		$this->submit_text = 'Login';
@@ -16,7 +17,7 @@ class AuthOrmFrontend_Core_Form_Login extends FormManager {
 		
 		$values = $this->get_input();
 		$auth = Auth::instance();
-		$success = $auth->login($values['username'], $values['password']);
+		$success = $auth->login($values['username'], $values['password'], (bool)$values['remember']);
 		if (!$success) $this->submit_status = self::SUBMIT_STATUS_FAIL;
 		$this->fields['password']['value'] = '';
 		
